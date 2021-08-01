0ad95d346400a50388a21ba6ddb7f474cadd7c6b
pp3times <than2314@gmail.com>
2021-08-01 17:28:02 +0700

    :zap: edit speak js

--- public/speak.js
+++ public/speak.js
@@ -1,6 +1,6 @@
 var synth = window.speechSynthesis;
 speak = () => {
-  var x = document.querySelector("#showText").value;
+  var x = document.querySelector("#showText").innerHTML;
   var utterThis = new SpeechSynthesisUtterance(x);
   utterThis.lang = "th-TH";
   synth.speak(utterThis);
