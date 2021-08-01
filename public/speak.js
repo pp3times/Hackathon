2bf95ae349dad2f9ea72680ab8fe3cbbcc2ec013
pp3times <than2314@gmail.com>
2021-08-01 16:58:20 +0700

    :zap: add text to speech v.2

--- public/speak.js
+++ public/speak.js
@@ -1,6 +1,6 @@
 var synth = window.speechSynthesis;
 speak = () => {
-  var x = document.querySelector("#showText").value;
+  var x = document.getElementById("showText").value;
   var utterThis = new SpeechSynthesisUtterance(x);
   utterThis.lang = "th-TH";
   synth.speak(utterThis);
