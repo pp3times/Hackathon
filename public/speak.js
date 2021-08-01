04ed17f67d787669e714d7295efb16cd814efc9d
Apichet Komwatcharapong <54273960+NewNeng@users.noreply.github.com>
2021-08-01 17:11:57 +0700

    Edit speak.js

--- public/speak.js
+++ public/speak.js
@@ -1,6 +1,7 @@
 var synth = window.speechSynthesis;
 speak = () => {
-  var x = document.querySelector("#showText").value;
+  // var x = document.querySelector("#showText").value;
+  var x = $('#showText').val();
   var utterThis = new SpeechSynthesisUtterance(x);
   utterThis.lang = "th-TH";
   synth.speak(utterThis);
